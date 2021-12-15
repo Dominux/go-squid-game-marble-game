242a338ac885a998b2495efec0a9c9811bfc3767
Dominux <kir.gimranov@gmail.com>
2021-12-15 23:26:20 +0300

    Added posibility to silently write data into terminal

--- internal/game_interactors/terminal_game_interactor.go
+++ internal/game_interactors/terminal_game_interactor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"syscall"
 )
 
 type TerminalGameInteractor struct {
@@ -18,10 +19,50 @@ func NewTerminalGameInteractor() *TerminalGameInteractor {
 func (t *TerminalGameInteractor) GetString(prompt string) (string, error) {
 	fmt.Println(prompt)
 
+	// Common settings and variables for both stty calls.
+	attrs := syscall.ProcAttr{
+		Dir:   "",
+		Env:   []string{},
+		Files: []uintptr{os.Stdin.Fd(), os.Stdout.Fd(), os.Stderr.Fd()},
+		Sys:   nil,
+	}
+	var ws syscall.WaitStatus
+
+	// Disable echoing.
+	pid, err := syscall.ForkExec(
+		"/bin/stty",
+		[]string{"stty", "-echo"},
+		&attrs,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the stty process to complete.
+	_, err = syscall.Wait4(pid, &ws, 0, nil)
+	if err != nil {
+		panic(err)
+	}
+
 	// Reading from stdin
 	str, err := t.reader.ReadString('\n')
 	if err != nil {
 		return "", err
+	}
+
+	// Re-enable echo.
+	pid, err = syscall.ForkExec(
+		"/bin/stty",
+		[]string{"stty", "echo"},
+		&attrs)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the stty process to complete.
+	_, err = syscall.Wait4(pid, &ws, 0, nil)
+	if err != nil {
+		panic(err)
 	}
 
 	// Removing "\n" from the end of the string and returning it
